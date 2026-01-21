e8f5dbe07fb4bf05a5f3be6715ca1c778b06d26e
Maria Vorobeva <tirimaria@gmail.com>
2026-01-21 15:26:26 +0000

    Add track ID from the database to /recommendations output (for front-end usage only)

--- api/main.py
+++ api/main.py
@@ -52,7 +52,7 @@ def read_item(mood_id: str, q: Union[str, None] = None):
     with psycopg.connect(DB_DSN) as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "select t.title, t.artist, t.genre, t.emotify_id from Tracks as t inner join Recommendations as r on t.id=r.track_id where r.mood_id=%s order by r.final_score desc;",
+                "select t.id, t.title, t.artist, t.genre, t.emotify_id from Tracks as t inner join Recommendations as r on t.id=r.track_id where r.mood_id=%s order by r.final_score desc;",
                 (mood_id,),
             )
             tracks = cur.fetchall()
@@ -60,12 +60,13 @@ def read_item(mood_id: str, q: Union[str, None] = None):
             tracks_output = []
 
             for track in tracks:
-                genre = track[2]
+                genre = track[3]
                 dict = {}
-                dict["title"] = track[0]
-                dict["artist"] = track[1]
+                dict["id"] = track[0]
+                dict["title"] = track[1]
+                dict["artist"] = track[2]
                 dict["genre"] = genre
-                filename = track[3].split("_")[1]
+                filename = track[4].split("_")[1]
                 dict["filepath"] = f"/mp3/{genre}/{filename}.mp3"
                 tracks_output.append(dict)
 
